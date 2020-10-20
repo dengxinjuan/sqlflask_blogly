059a03654d9acdfafd4146c513d235e1449adde4
xinjuan Deng <dengxinj@gmail.com>
2020-10-19 20:29:27 -0400

    add test

--- test_blogly.py
+++ test_blogly.py
@@ -22,12 +22,16 @@ class UserModelTestCase(TestCase):
     def setup(self):
         """clean up all existing user"""
         User.query.delete()
+        user=User(first_name="xinjuan",last_name="deng",image_url=None)
+        db.session.add(user)
+        db.session.commit()
+        self.id = user.id
 
     def tearDown(self):
         """clean up any fouled transcation"""
         db.session.rollback()
     
-    def test_getfullname(self):
+    def test_fullname(self):
         """test get full name function"""
         user=User(first_name="xinjuan",last_name="deng",image_url=None)
         self.assertEquals(user.get_full_name(),"xinjuandeng")
@@ -35,15 +39,33 @@ class UserModelTestCase(TestCase):
     def test_userhome(self):
         with app.test_client() as client:
             """test user name in home page"""
-            user=User(first_name="xinjuan",last_name="deng",image_url=None)
-            db.session.add(user)
-            db.session.commit()
-
             resp = client.get("/users")
             html = resp.get_data(as_text=True)
             self.assertEqual(resp.status_code,200)
             self.assertIn("xinjuandeng",html)
 
+    def test_adduser(self):
+        """test add new user"""
+        deng = User.query.filter(last_name="deng")
+        self.assertEquals(deng.last_name,"deng")
+    
+     def test_form(self):
+        with app.test_client() as client:
+            """test form in form page"""
+            resp = client.get("/users/new")
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code,200)
+            self.assertIn("<form></form>",html)
+        
+    def test_show_user(self):
+        with app.test_client() as client:
+        """test user detail page"""
+            resp = client.get("f"/users/{user.id}")
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code,200)
+            self.assertIn("xinjuandeng",html)
+
+    
 
 
 
